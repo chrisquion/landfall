dd894afcda2481689f3e24d9e547dbe533b15607
Chris Quion <chrischrisquion@gmail.com>
2020-07-22 13:54:50 -0400

    button now changes color on click

--- public/app.js
+++ public/app.js
@@ -4,43 +4,44 @@ $( document ).ready(function() {
   const isCollapsed = "collapsed";
   const menuLinks = document.querySelectorAll(".employee-menu a");
   const toggleMobMenu = document.querySelector(".toggle-mob-menu");
-  const articleBadge = document.querySelectorAll("article-badge");
-
+  const articleBadges = document.getElementsByClassName("article-badge-button");
 
   collapseButton.addEventListener("click", function() {
-      this.getAttribute("aria-expanded") == "true"
-        ? this.setAttribute("aria-expanded", "false")
-        : this.setAttribute("aria-expanded", "true");
-      this.getAttribute("aria-label") == "collapse menu"
-        ? this.setAttribute("aria-label", "expand menu")
-        : this.setAttribute("aria-label", "collapse menu");
-      body.classList.toggle(isCollapsed);
+    this.getAttribute("aria-expanded") == "true"
+      ? (this.setAttribute("aria-expanded", "false"), console.log("menu collapsed"))
+      : this.setAttribute("aria-expanded", "true");
+    this.getAttribute("aria-label") == "collapse menu"
+      ? this.setAttribute("aria-label", "expand menu")
+      : (this.setAttribute("aria-label", "collapse menu"), console.log("menu opened"));
+    body.classList.toggle(isCollapsed);
   });
 
   toggleMobMenu.addEventListener("click", function() {
-      this.getAttribute("aria-expanded") == "true" 
-          ? this.setAttribute("aria-expanded", "false")
-          : this.setAttribute("aria-expanded", "true");
-      this.getAttribute("aria-label") == "open menu"
-          ? this.setAttribute("aria-label", "close menu")
-          : this.setAttribute("aria-label", "open menu");
-      body.classList.toggle("mob-menu-opened");
+    this.getAttribute("aria-expanded") == "true" 
+        ? (this.setAttribute("aria-expanded", "false"), console.log("mob-menu collapsed"))
+        : this.setAttribute("aria-expanded", "true");
+    this.getAttribute("aria-label") == "open menu"
+        ? this.setAttribute("aria-label", "close menu")
+        : (this.setAttribute("aria-label", "open menu"), console.log("mob-menu opened"));
+    body.classList.toggle("mob-menu-opened");
   });
 
   for (const link of menuLinks) {
-      link.addEventListener("mouseenter", function() {
-        body.classList.contains(isCollapsed) &&
-        window.matchMedia("(min-width: 768px)").matches
-          ? this.setAttribute("title", this.textContent)
-          : this.removeAttribute("title");
-      });
+    link.addEventListener("mouseenter", function() {
+      body.classList.contains(isCollapsed) &&
+      window.matchMedia("(min-width: 768px)").matches
+        ? this.setAttribute("title", this.textContent)
+        : this.removeAttribute("title");
+    });
   }
-  
+ 
+
   for (const badge of articleBadges) {
-      badge.addEventListner("click", function() {
-        this.getAttribute("aria-label") == "badge button orange"
-          ? this.setAttribute("aria-label", "badge button blue")
-          : this.setAttribute("aria-label", "badge button orange");
-      });
+    console.log("badge");
+    badge.addEventListener("click", function() {
+      this.getAttribute("aria-label") == "badge button orange"
+        ? (this.setAttribute("aria-label", "badge button blue"), $(this).find('svg').css("fill", "blue"))
+        : (this.setAttribute("aria-label", "badge button orange"), $(this).find('svg').css("fill", "orange"));
+    });
   }
 });
